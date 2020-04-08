5614617c0a500719b5be6f66864b7688cec9bc3b
mRcfps <1402491442@qq.com>
2020-04-08 20:31:46 +0800

    useState 与 useEffect：再巩固，再强化

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React, { useState, useEffect } from "react";
 
 import "./App.css";
 import GlobalStats from "./components/GlobalStats";
+import CountriesChart from "./components/CountriesChart";
+import SelectDataKey from "./components/SelectDataKey";
 
 const BASE_URL = "https://corona.lmao.ninja";
 
 function App() {
   const [globalStats, setGlobalStats] = useState({});
+  const [countries, setCountries] = useState([]);
+  const [key, setKey] = useState("cases");
 
   useEffect(() => {
     const fetchGlobalStats = async () => {
@@ -21,10 +25,22 @@ function App() {
     return () => clearInterval(intervalId);
   }, []);
 
+  useEffect(() => {
+    const fetchCountries = async () => {
+      const response = await fetch(`${BASE_URL}/countries?sort=${key}`);
+      const data = await response.json();
+      setCountries(data.slice(0, 10));
+    };
+
+    fetchCountries();
+  }, [key]);
+
   return (
     <div className='App'>
       <h1>COVID-19</h1>
       <GlobalStats stats={globalStats} />
+      <SelectDataKey onChange={(e) => setKey(e.target.value)} />
+      <CountriesChart data={countries} dataKey={key} />
     </div>
   );
 }
---
+++ src/components/CountriesChart.js
@@ -0,0 +1,31 @@
+import React from "react";
+import {
+  BarChart,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Legend,
+  Bar,
+} from "recharts";
+
+function CountriesChart({ data, dataKey }) {
+  return (
+    <BarChart
+      width={1200}
+      height={250}
+      style={{ margin: "auto" }}
+      margin={{ top: 30, left: 20, right: 30 }}
+      data={data}
+    >
+      <CartesianGrid strokeDasharray='3 3' />
+      <XAxis dataKey='country' />
+      <YAxis />
+      <Tooltip />
+      <Legend />
+      <Bar dataKey={dataKey} fill='#8884d8' />
+    </BarChart>
+  );
+}
+
+export default CountriesChart;
