20de81a9f805666a8344c0ec4c4f12ce07dd24b8
mRcfps <1402491442@qq.com>
2020-04-10 15:02:01 +0800

    自定义 Hook：量身定制

--- src/App.js
+++ src/App.js
@@ -1,39 +1,23 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
 import "./App.css";
 import GlobalStats from "./components/GlobalStats";
 import CountriesChart from "./components/CountriesChart";
 import SelectDataKey from "./components/SelectDataKey";
-
-const BASE_URL = "https://corona.lmao.ninja";
+import { useCoronaAPI } from "./hooks/useCoronaAPI";
 
 function App() {
-  const [globalStats, setGlobalStats] = useState({});
-  const [countries, setCountries] = useState([]);
+  const globalStats = useCoronaAPI("/all", {
+    initialData: {},
+    refetchInterval: 5000,
+  });
+
   const [key, setKey] = useState("cases");
-
-  useEffect(() => {
-    const fetchGlobalStats = async () => {
-      const response = await fetch(`${BASE_URL}/all`);
-      const data = await response.json();
-      setGlobalStats(data);
-    };
-
-    fetchGlobalStats();
-    const intervalId = setInterval(fetchGlobalStats, 5000);
-
-    return () => clearInterval(intervalId);
-  }, []);
-
-  useEffect(() => {
-    const fetchCountries = async () => {
-      const response = await fetch(`${BASE_URL}/countries?sort=${key}`);
-      const data = await response.json();
-      setCountries(data.slice(0, 10));
-    };
-
-    fetchCountries();
-  }, [key]);
+  const countries = useCoronaAPI(`/countries?sort=${key}`, {
+    initialData: [],
+    deps: [key],
+    converter: (data) => data.slice(0, 10),
+  });
 
   return (
     <div className='App'>
---
+++ src/hooks/useCoronaAPI.js
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react";
+
+const BASE_URL = "https://corona.lmao.ninja";
+
+export function useCoronaAPI(
+  path,
+  {
+    initialData = null,
+    deps = [],
+    converter = (data) => data,
+    refetchInterval = null,
+  }
+) {
+  const [data, setData] = useState(initialData);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(`${BASE_URL}${path}`);
+      const data = await response.json();
+      setData(converter(data));
+    };
+    fetchData();
+
+    if (refetchInterval) {
+      const intervalId = setInterval(fetchData, refetchInterval);
+      return () => clearInterval(intervalId);
+    }
+  }, [converter, path, refetchInterval]);
+
+  return data;
+}
